98fd0505d49ed4f0f1a2faa934b8f186b5b5ea36
Mahendra <mahendradewangan195@gmail.com>
2023-04-15 00:23:07 +0530

    updated styles and changed about us, contact us, work, project file to ejs file

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
+
 // app.use(express.static('public'));
 app.use(express.static(__dirname + "/public/"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.set('view engine', 'ejs');
+
+// app.set('view engine', 'ejs');
 
 
 
@@ -13,6 +18,21 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
+app.get("/work", function(req, res){
+    // res.sendFile(__dirname + "/work.html");
+    res.render("work");
+});
+app.get("/project", function(req, res){
+    // res.sendFile("project");
+    res.render("project")
+});
+app.get("/about", function(req, res){
+    res.render("about");
+});
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
+
 
 
 app.listen(3000, function(){
