3b913d468d7271f898e95dc7104e6792e958275c
Mahendra <mahendradewangan195@gmail.com>
2023-08-31 00:09:47 +0530

    ui adjustments

--- public/js/profile-photo-border.js
+++ public/js/profile-photo-border.js
@@ -1,16 +1,16 @@
-let box = document.querySelector('.profile-photo');
-let height = box.offsetHeight;
-console.log("height of the photo is : " + height);
+// let box = document.querySelector('.profile-photo');
+// let height = box.offsetHeight;
+// console.log("height of the photo is : " + height);
 
-document.querySelector('.border-box').style.height = height;
+// document.querySelector('.border-box').style.height = height;
 
-function changeHeight(){
-    let ele = document.getElementById("profile-photo-border");
-    ele.style.height = `${height}px`;
-    // ele.style.height = height+'px';
-}
+// function changeHeight(){
+//     let ele = document.getElementById("profile-photo-border");
+//     ele.style.height = `${height}px`;
+//     // ele.style.height = height+'px';
+// }
 
-changeHeight();
+// changeHeight();
 
 
 
