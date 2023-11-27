90f39e383e5ea61ad6f911b99751593793d80e8f
Mahendra <mahendradewangan195@gmail.com>
2023-11-27 22:52:29 +0530

    added type writter animation

--- public/js/script.js
+++ public/js/script.js
@@ -40,3 +40,52 @@
 
   new WOW().init()
 
+ 
+  // Text typing animation
+  var words = ['I make brands that matters. ', 'I design & build digital arts.', 'I build things for the web. ', 'Turning ideas into Code. '];
+  var part;
+  var i = 0;
+  var offset = 0;
+  var len = words.length;
+  var forwards = true;
+  var skip_count = 0;
+  var skip_delay = 15;
+  var speed = 70;
+
+  var wordflick = function () {
+    setInterval(function () {
+      if (forwards) {
+        if (offset >= words[i].length) {
+          ++skip_count;
+          if (skip_count == skip_delay) {
+            forwards = false;
+            skip_count = 0;
+          }
+        }
+      }
+      else {
+        if (offset == 0) {
+          forwards = true;
+          i++;
+          offset = 0;
+          if (i >= len) {
+            i = 0;
+          }
+        }
+      }
+      part = words[i].substr(0, offset);
+      if (skip_count == 0) {
+        if (forwards) {
+          offset++;
+        }
+        else {
+          offset--;
+        }
+      }
+      $('.typewriter-animation').text(part);
+    },speed);
+  };
+  
+  $(document).ready(function () {
+    wordflick();
+  });
